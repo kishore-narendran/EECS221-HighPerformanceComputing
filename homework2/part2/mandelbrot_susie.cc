5484ac90b55875b12a16b7c8c59fb9aa5fb44e35
Kishore Narendran <kishore.narendran09@gmail.com>
2016-04-29 00:47:32 -0700

    Added simple MPI hello world

--- homework2/part2/mandelbrot_susie.cc
+++ homework2/part2/mandelbrot_susie.cc
@@ -7,10 +7,16 @@
 #include <iostream>
 #include <cstdlib>
 
-int
-main (int argc, char* argv[])
+int main (int argc, char* argv[])
 {
   /* Lucky you, you get to write MPI code */
+  MPI_Init (&argc, &argv);	/* starts MPI */
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* Get process id */
+  MPI_Comm_size (MPI_COMM_WORLD, &np);	/* Get number of processes */
+  MPI_Get_processor_name (hostname, &namelen); /* Get hostname of node */
+  printf ("Hello, world! [Host:%s -- Rank %d out of %d]\n", hostname, rank, np);
+  MPI_Finalize();
+  return 0;
 }
 
 /* eof */

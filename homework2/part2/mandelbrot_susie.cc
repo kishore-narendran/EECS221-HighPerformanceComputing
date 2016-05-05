68f153b4f1a960e93e5fa39e4fc630935c1e923c
Kishore Narendran <kishore.narendran09@gmail.com>
2016-05-05 13:12:43 -0700

    Some minor changes

--- homework2/part2/mandelbrot_susie.cc
+++ homework2/part2/mandelbrot_susie.cc
@@ -119,7 +119,7 @@ auto img_view = gil::view(img);
       }
       for(int j = 0; j < width; j++)
       {
-        final_image[np*y+x][j] = recv_buffer[i*width+j];
+        final_image[(np*y)+x][j] = recv_buffer[i*width+j];
       }
       y++;
     }
